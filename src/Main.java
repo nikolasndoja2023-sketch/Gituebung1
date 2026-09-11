92c4f2f674f529c004da71e8205ae65b967aa98b
NikolasNdoja <nikndo19@htl-shkoder.com>
2026-09-11 09:18:44 +0200

    Merge confilct

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
 
-        String name= "Nikolas";
-        System.out.println("Hello" + name);
+String name= "Nikolas";
+String surname= "Ndoja";
+
+        System.out.println("Hello "+name + surname);
 
 
     }
